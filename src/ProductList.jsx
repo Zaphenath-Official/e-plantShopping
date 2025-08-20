fe647c086f9940aee4ec47a92f403f466a4cabc6
Zaphenath-Official <zaphenathofficial@gmail.com>
2025-08-21 00:44:43 +0300

    #

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -8,7 +8,9 @@ function ProductList({ onHomeClick }) {
     const [showPlants, setShowPlants] = useState(true); // State to control visibility of plants list
     
     const cartItems = useSelector((state) => state.cart.items);
-    const cartItemsLength = cartItems.length; // Get the length of cart items
+    const totalItemsInCart = cartItems.reduce((acc, item) => {
+      return acc + item.quantity; // Calculate total items in the cart
+    }, 0);
     const dispatch = useDispatch();
 
     const plantsArray = [
@@ -281,7 +283,7 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#products-list" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#cart" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg>{cartItemsLength > 0 && <span className="cart_quantity_count">{cartItemsLength}</span>}</h1></a></div>
+                    <div> <a href="#cart" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg>{totalItemsInCart > 0 && <span className="cart_quantity_count">{totalItemsInCart}</span>}</h1></a></div>
                 </div>
             </div>
             {!showCart ? (
